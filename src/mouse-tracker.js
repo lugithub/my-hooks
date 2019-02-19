223e4abede317550d82cef2981273b4679aa073d
Bin Lu <lu.bin.lu.bin@gmail.com>
2019-02-19 17:11:02 -0500

    another useMouse that returns only mouse state

--- src/mouse-tracker.js
+++ src/mouse-tracker.js
@@ -45,30 +45,30 @@ function Counter() {
   return <h1 style={{ color }}>{count}</h1>;
 }
 
-function useMouse(rp) {
-  const [mouse, setMouse] = useState({ x: 0, y: 0, id: 0 });
+function useMouse() {
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
-  function handleMouseMove(event) {
-    setMouse({
-      ...mouse,
-      x: event.clientX,
-      y: event.clientY
-    });
-  }
-  return (
-    <div style={{ height: '300px' }} onMouseMove={handleMouseMove}>
-      {rp(mouse)}
-    </div>
-  );
+  return [mouse, setMouse];
 }
 
 export default function MouseTracker({ showCounter }) {
+  const [mouse, setMouse] = useMouse();
+
   return (
     <div>
       <h1>Move the mouse around!</h1>
-      {useMouse(mouse => (
+      <div
+        style={{ height: '300px' }}
+        onMouseMove={event => {
+          setMouse({
+            ...mouse,
+            x: event.clientX,
+            y: event.clientY
+          });
+        }}
+      >
         <Cat mouse={mouse} />
-      ))}
+      </div>
       {showCounter ? <Counter /> : null}
     </div>
   );
