bce707535b1cb8a7f97dd8e6043a6a2ab249d551
lubin <lu.bin.lu.bin@gmail.com>
2019-02-24 22:19:06 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { Link, Route } from 'react-router-dom';
 import ColorContext from './color-context';
 import MouseTracker from './mouse-tracker';
 import useLocation from './use-location';
 import Counter from './counter';
+import './App.css';
 
 function App() {
   const [showCounter, setShowCounter] = useState(true);
@@ -12,23 +14,42 @@ function App() {
 
   return (
     <div className="App">
+      <nav className="nav">
+        <Link to="/counter-without-context-provider">
+          counter-without-context-provider
+        </Link>
+        <Link to="/mouse-tracker">mouse-tracker</Link>
+      </nav>
+
       <p>{location}</p>
 
-      <ColorContext.Provider value={color}>
-        <MouseTracker showCounter={showCounter} />
-      </ColorContext.Provider>
-      <Counter />
+      <div>
+        <Route path="/counter-without-context-provider" component={Counter} />
+      </div>
 
-      <button onClick={() => setShowCounter(!showCounter)}>
-        toggle showCounter
-      </button>
-      <button
-        onClick={() => {
-          setColor(prev => (prev === 'red' ? 'green' : 'red'));
-        }}
-      >
-        toggle theme
-      </button>
+      <Route path="/mouse-tracker">
+        {({ match }) =>
+          match && (
+            <Fragment>
+              <ColorContext.Provider value={color}>
+                <MouseTracker showCounter={showCounter} />
+              </ColorContext.Provider>
+
+              <button onClick={() => setShowCounter(!showCounter)}>
+                toggle showCounter
+              </button>
+
+              <button
+                onClick={() => {
+                  setColor(prev => (prev === 'red' ? 'green' : 'red'));
+                }}
+              >
+                toggle theme
+              </button>
+            </Fragment>
+          )
+        }
+      </Route>
     </div>
   );
 }
